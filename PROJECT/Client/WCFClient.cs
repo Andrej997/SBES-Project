b7cadf231e567ccc901d3914c176ec6641ca07d9
andrej <andrej.km997@gmail.com>
2019-12-06 07:30:27 +0100

    start
    Kreirao sam samo 4 projekta u kojima cemo raditi od ponedeljka

---
+++ PROJECT/Client/WCFClient.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class WCFClient : ChannelFactory<IWCFContract>, IWCFContract, IDisposable
+    {
+        IWCFContract factory;
+        public void TestCommunication()
+        {
+            try
+            {
+                factory.TestCommunication();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[TestCommunication] ERROR = {0}", e.Message);
+            }
+        }
+    }
+}
---
+++ PROJECT/ServiceManagement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Contracts;
+using System.ServiceModel.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace ServiceManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process notePad = new Process();
+            notePad.StartInfo.FileName = "notepad.exe";
+            //notePad.StartInfo.Arguments = "mytextfile.txt";
+            notePad.Start();
+        }
+    }
+}
