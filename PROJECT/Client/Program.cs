8b6e08eed6907e52fa1796fc23ebe375a1cd47bf
andrej <andrej.km997@gmail.com>
2019-12-09 11:58:14 +0100

    Nadogradnja

--- PROJECT/Client/Program.cs
+++ PROJECT/Client/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Manager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,56 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            // servisni sertifitak koji ocekujemo
+            string srvCertCN = "WCFSrvice";
+
+            // sertifikat koji ocekujemo za povezivanje
+            string signCertCN = String.Empty;
+
+            string wrongCertCN = String.Empty;
+
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            /// Use CertManager class to obtain the certificate based on the "srvCertCN" representing the expected service identity.
+			X509Certificate2 srvCert = CertManager.GetCertificateFromStorage(
+                                    StoreName.TrustedPeople,
+                                    StoreLocation.LocalMachine, 
+                                    srvCertCN);
+            EndpointAddress address = new EndpointAddress(new Uri("net.tcp://localhost:9999/Receiver"),
+                                      new X509CertificateEndpointIdentity(srvCert));
+
+            using (WCFClient proxy = new WCFClient(binding, address))
+            {
+                /// 1. Communication test
+                proxy.TestCommunication();
+                Console.WriteLine("TestCommunication() finished. Press <enter> to continue ...");
+                Console.ReadLine();
+
+                /// 2. Digital Signing test				
+                string message = "Exercise 02";
+
+                /// Create a signature based on the "signCertCN"
+                X509Certificate2 signCert = null;
+
+                /// Create a signature using SHA1 hash algorithm
+                //byte[] signature = DigitalSignature.Create();
+                //proxy.SendMessage();
+
+                Console.WriteLine("SendMessage() using {0} certificate finished. Press <enter> to continue ...", signCertCN);
+                Console.ReadLine();
+
+
+                /// For the same message, create a signature based on the "wrongCertCN"
+                X509Certificate2 wrongSignCert = null;
+
+                /// Create a signature using SHA1 hash algorithm
+                //byte[] signature1 = DigitalSignature.Create();
+                //proxy.SendMessage();
+
+                Console.WriteLine("SendMessage() using {0} certificate finished. Press <enter> to continue ...", wrongCertCN);
+                Console.ReadLine();
+            }
         }
     }
 }
--- PROJECT/Client/WCFClient.cs
+++ PROJECT/Client/WCFClient.cs
@@ -5,12 +5,32 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using Manager;
+using System.Security.Principal;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Client
 {
     public class WCFClient : ChannelFactory<IWCFContract>, IWCFContract, IDisposable
     {
         IWCFContract factory;
+
+        public WCFClient(NetTcpBinding binding, EndpointAddress address)
+            : base(binding, address)
+        {
+            /// cltCertCN.SubjectName should be set to the client's username. .NET WindowsIdentity class provides information about Windows user running the given process
+            string cltCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
+            this.Credentials.ServiceCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.Custom;
+            this.Credentials.ServiceCertificate.Authentication.CustomCertificateValidator = new AuditCertValidator();
+            this.Credentials.ServiceCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            /// Set appropriate client's certificate on the channel. Use CertManager class to obtain the certificate based on the "cltCertCN"
+            this.Credentials.ClientCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, cltCertCN);
+
+            factory = this.CreateChannel();
+        }
+
         public void TestCommunication()
         {
             try
